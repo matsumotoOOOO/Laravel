ac1941b55901578394e4f073c911bbf229854279
miho <m.seim.m01@gmail.com>
2021-10-16 04:08:19 +0000

    first commit

---
+++ app/Http/Controllers/Admin/NewsController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class NewsController extends Controller
+{
+    //
+public function add() 
+{
+return view ('admin.news.create');    
+}    
+    
+    
+    
+}
+
+
