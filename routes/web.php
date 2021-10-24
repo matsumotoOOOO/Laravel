8a1ee514add1e88ba14ad418522af59d0741f3f7
miho <m.seim.m01@gmail.com>
2021-10-24 01:48:58 +0000

    second commit

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,21 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
+Route::group(['prefix' => 'admin', 'middleware' => 'auth' ], function() {
+    Route::get('news/create', 'Admin\NewsController@add') ;
+    Route::post('news/create', 'Admin\NewsController@create'); # 追記
 });
 Route::get('xxx','AAAController@bbb');
 
-Route::group(['prefix' => 'admin' ], function() {
+Route::group(['prefix' => 'admin','middleware' => 'auth'], function() {
     Route::get('profile/create','Admin\ProfileController@add');
     Route::get('profile/edit','Admin\ProfileController@edit');
+    Route::post('profile/create','Admin\ProfileController@create');
+    Route::post('profile/edit','Admin\ProfileController@edit');
+
+    
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
